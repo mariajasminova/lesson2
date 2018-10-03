1a9d4704cbf145bc216c3d0d683d129f757258bc
mariajasminova <masha.petrashova@yandex.ru>
2018-10-03 15:59:05 +0300

    Added file 'class_human.php'
    Добавлен файл с описание класса 'Human'

---
+++ class_human.php
@@ -0,0 +1,21 @@
+class Human 
+{ 
+	protected $firstName;
+	protected $lastName;
+	protected $age;
+
+public function setFullName($firstName, $lastName, $age)
+{
+	$this->firstName = $firstName;
+	$this->lastName = $lastName;
+	$this->age = $age;
+
+}
+
+public function getFullName()
+{
+	return $this->firstName . ' ' . $this->lastName . ' ' . $this->age;
+
+}
+
+}
